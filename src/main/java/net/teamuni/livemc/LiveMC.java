cdf72be1d32522e55cd5a1894a8509ae97867909
Kari <carrie7280@gmail.com>
2022-01-04 23:31:50 +0900

    Initial commit

---
+++ src/main/java/net/teamuni/livemc/LiveMC.java
@@ -0,0 +1,16 @@
+package net.teamuni.livemc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LiveMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
